04d0da3aded644df19cd58a9a8e0894d652df4ff
Yang <yang@YangmatoiMac.local>
2012-06-08 13:55:18 +0800

    Initial Commit

---
+++ Custom Cell/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Custom Cell
+//
+//  Created by Yang on 12-6-8.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Custom Cell/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Custom Cell
+//
+//  Created by Yang on 12-6-8.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
